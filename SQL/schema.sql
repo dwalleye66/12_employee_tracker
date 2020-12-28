41ae9fbf433dcd193e48071b83c9e7095af2768e
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2020-12-28 16:36:04 -0600

    added initial inquirer logic for basic requirements

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL
  ,title VARCHAR(30) NOT NULL
- ,salary DECIMAL(10,2) NOT NULL
+ ,salary DECIMAL(10,4) NOT NULL
  ,department_id INT NOT NULL
  ,PRIMARY KEY (id)
 );
--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 // Required modules
 const connection = require("./connection");
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 
 // Connect to database and execute inquirer module
 connection.connect((err) => {
     if (err) throw err;
-    viewEmployees();
+    start();
 });
 
+// Prompts user for desired action
+const start = () => {
+    inquirer
+        .prompt({
+            name: "chooseAction",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Employee Role"]
+        })
+        .then(function (answer) {
+            // Execute correct function based on user's answer
+            if (answer.chooseAction === "View All Departments") {
+                viewDepartments();
+            }
+            else if (answer.chooseAction === "View All Roles") {
+                viewRoles();
+            }
+            else if (answer.chooseAction === "View All Employees") {
+                viewEmployees();
+            } else {
+                connection.end();
+            }
+        });
+}
+
+// Query the database and return all employees for viewing
 const viewEmployees = () => {
     connection.query('SELECT * FROM employee;', (err, results) => {
-        console.table(results); // results contains rows returned by server
+        if (err) throw err;
+        // Display results of query in a table format
+        console.table(results);
+        // Return user to initial questions for further actions
+        start();
     })
 }

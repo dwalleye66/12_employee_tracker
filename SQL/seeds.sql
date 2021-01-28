67ed756fb6162041af5f02ac13d65d709b018c60
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-28 13:04:36 -0600

    udpated schema and seeds with salary update. also added ability to add a new department and new role

--- SQL/seeds.sql
+++ SQL/seeds.sql
@@ -1,10 +1,18 @@
 USE employee_tracker_db;
 
 INSERT INTO department (name)
-VALUES ("HR");
+VALUES 
+    ("Human Resources"),
+    ("Operations"),
+    ("Accounting"),
+    ("Information Technology");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 50000.00,1);
+VALUES 
+    ("Manager", 50000.00,1),
+    ("Clerk", 30000.00,1);
 
-INSERT INTO employee (first_name,last_name,role_id)
-VALUES ("Timothy","Kemp",1);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES 
+    ("Timothy","Kemp",1,NULL),
+    ("Mickey","Mouse",2,1);
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const start = () => {
                 "Update Employee Role",
                 "EXIT"]
         })
-        .then(function (answer) {
+        .then((answer) => {
             // Execute correct function based on user's answer
             switch (answer.chooseAction) {
                 case "View All Departments":
@@ -81,11 +81,158 @@ const viewRoles = () => {
 
 // Query the database and return all employees for viewing
 const viewEmployees = () => {
-    connection.query('SELECT * FROM employee;', (err, results) => {
-        if (err) throw err;
-        // Display results of query in a table format
-        console.table(results);
-        // Return user to initial questions for further actions
-        start();
-    })
+    connection.query(`
+        SELECT 
+          employee.id
+         ,employee.first_name
+         ,employee.last_name
+         ,role.title
+         ,department.name AS department
+         ,role.salary
+         ,CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+        FROM employee 
+        LEFT JOIN role ON employee.role_id = role.id 
+        LEFT JOIN department ON role.department_id = department.id 
+        LEFT JOIN employee AS manager ON manager.id = employee.manager_id;`,
+        (err, results) => {
+            if (err) throw err;
+            // Display results of query in a table format
+            console.table(results);
+            // Return user to initial questions for further actions
+            start();
+        })
 }
+
+// Prompts user for name of new department to add; adds department to database
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: "departmentName",
+                type: "input",
+                message: "What is the name of the department you'd like to add?"
+            }
+        ])
+        .then((answer) => {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.departmentName
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log("The department was created successfully!");
+                    // Return user to initial questions for further actions
+                    start();
+                }
+            );
+        });
+};
+
+// Prompts user for name, salary, and department of new role to add; adds department to database
+const addRole = () => {
+    connection.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "roleName",
+                    type: "input",
+                    message: "What is the name of the role you'd like to add?"
+                },
+                {
+                    name: "salary",
+                    type: "input",
+                    message: "What is the salary for this role?",
+                    validate: (value) => {
+                        if (isNaN(value) === false && value > 0) {
+                            return true;
+                        }
+                        console.log(" <-- Value must be a number greater than 0!");
+                        return false;
+                    }
+                },
+                {
+                    name: "department",
+                    type: "list",
+                    message: "Which department does this role belong to?",
+                    choices: () => {
+                        let choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].name);
+                        }
+                        return choiceArray;
+                    }
+                }
+            ])
+            .then((answer) => {
+                let chosenDepartmentID;
+
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].name === answer.department) {
+                        chosenDepartmentID = results[i].id;
+                    }
+                }
+
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    {
+                        title: answer.roleName,
+                        salary: answer.salary,
+                        department_id: chosenDepartmentID
+                    },
+                    (err) => {
+                        if (err) throw err;
+                        console.log("The role was created successfully!");
+                        // Return user to initial questions for further actions
+                        start();
+                    }
+                );
+            });
+    });
+};
+
+
+// Query the database and return all employees for viewing
+// const addEmployee = () => {
+//     inquirer.prompt(
+//         {
+//             name: "firstName",
+//             type: "input",
+//             message: "What is the employee's first name?"
+//         }
+//     );
+
+//     const { lastName } = await inquirer.prompt(
+//         {
+//             name: "lastName",
+//             type: "input",
+//             message: "What is the employee's last name?"
+//         }
+//     );
+
+//     await connection.query(
+//         `SELECT 
+//           employee.first_name
+//         , employee.last_name
+//         , role.title
+//         FROM employee 
+//         LEFT JOIN role ON employee.role_id = role.id 
+//         WHERE role.title = "Manager";`,
+//         (err, results) => {
+//             if (err) throw err;
+//             // Display results of query in a table format
+//             console.table(results)
+//         });
+
+//     await inquirer.prompt(
+//         {
+//             name: "manager",
+//             type: "list",
+//             message: "What is the employee's last name?"
+//         }
+//     );
+
+
+//     console.log('firstName, lastName :>> ', firstName, lastName);
+// }
